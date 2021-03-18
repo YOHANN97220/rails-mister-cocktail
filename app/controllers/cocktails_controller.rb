f42d4c667fb773c23933e3aace01b3f4d1c0ba9b
yohann paulin <yohannpaulin@hotmail.fr>
2021-03-18 21:52:10 +0100

    view implémentées

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,25 @@
 class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(params_cocktail)
+    @cocktail.save
+    redirect_to cocktails_path
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  private
+
+  def params_cocktail
+    params.require(:cocktail).permit(:name)
+  end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,17 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(params_dose)
+    @dose.save
+    redirect_to doses_path(@dose)
+  end
+
+  private
+
+  def params_dose
+    params.require('doses').permit(:cocktail, :ingredients)
+  end
 end
